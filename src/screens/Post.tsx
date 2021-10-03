428d31a6f72959cf11200bc4d1defeecf635f4cb
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-10-03 08:15:17 -0600

    Add simple screen for posts

--- src/screens/Post.tsx
+++ src/screens/Post.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Text, ActivityIndicator } from 'react-native';
+import { ScrollView, Text, ActivityIndicator } from 'react-native';
 import { RouteProp, useRoute } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { getEntry } from '../utils/entries';
 import { AppStackParamList } from '../types/navigation';
+import Markdown from 'react-native-markdown-display';
 
 const Post = () => {
   const { params } = useRoute<RouteProp<AppStackParamList, 'Post'>>();
@@ -21,8 +22,19 @@ const Post = () => {
   }, [getPost]);
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {loading ? <ActivityIndicator /> : <Text>{JSON.stringify(data)}</Text>}
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#FFFFFF' }}>
+      {loading || !data ? (
+        <ActivityIndicator />
+      ) : (
+        data && (
+          <ScrollView style={{ paddingHorizontal: 16 }}>
+            <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
+              {data.title}
+            </Text>
+            <Markdown>{data.body}</Markdown>
+          </ScrollView>
+        )
+      )}
     </SafeAreaView>
   );
 };
